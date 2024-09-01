0f6c3939c80b2ccb321033afb3cba230bcbba6c4
Vibhuti <vibhutigupta270@gmail.com>
2024-09-01 16:44:48 +0530

    verify-code

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -82,7 +82,7 @@ export async function GET(request:Request){
     }
     return Response.json({
         success: true,
-        isAcceptingMessage: foundUser.isAcceptingMessage
+        isAcceptingMessages: foundUser.isAcceptingMessages
     },
     {status: 200}
     )
